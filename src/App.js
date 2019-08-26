71c5479579188da54018b908bab6232ee9d10b40
chikechris <cchike2017@gmail.com>
2019-08-26 15:02:46 -0400

    basic form is working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import {Route} from 'react-router-dom'
-import EventsList from './pages/EventsList'
-import Register from './pages/Register'
-import AddEvent from './pages/AddEvent'
+import React from 'react'
+import { Route } from 'react-router-dom'
+// import EventsList from './pages/EventsList'
+// import Register from './pages/Register'
+import AddEvent from './pages/AddEvent/AddEvent'
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Corporate Event Planner</h1>
-      <Route exact path="/" component={EventsList}/>
-      <Route path="/register" component={Register}/>
-      <Route path="/event" component={Event}/>
-      <Route path="/addevent" component={AddEvent}/>
-
+      {/* <Route exact path='/' component={EventsList} />
+      <Route path='/register' component={Register} />
+      <Route path='/event' component={Event} /> */}
+      <Route path='/addevent' component={AddEvent} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
