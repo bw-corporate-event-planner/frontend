f7a8c988d9e2e1b9d4ebc2a933987ef639497ba0
lilyhoratio <lilyhzhou@gmail.com>
2019-08-26 10:23:57 -0700

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Route} from 'react-router-dom'
+import EventsList from './pages/EventsList'
+import Register from './pages/Register'
 
 function App() {
   return (
     <div className="App">
       <h1>Corporate Event Planner</h1>
+      <Route exact path="/" component={EventsList}/>
+      <Route path="/register" component={Register}/>
+      <Route path="/event" component={Event}/>
+      <Route path="/addevent" component={AddEvent}/>
+
     </div>
   );
 }
