f2c0dcf6318629cfb1bc1e6268443176f7c4dc37
chikechris <cchike2017@gmail.com>
2019-08-26 14:02:09 -0400

    app initialized

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route} from 'react-router-dom'
 import EventsList from './pages/EventsList'
 import Register from './pages/Register'
+import AddEvent from './pages/AddEvent'
 
 function App() {
   return (
