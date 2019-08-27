df6635ae5db7f62f9ceb9cdf0266cea2e52566bc
Annique Nsabimana <anniquensabimana@gmail.com>
2019-08-26 20:39:37 -0500

    created footer...not finished

--- src/App.js
+++ src/App.js
@@ -5,18 +5,29 @@ import Register from './pages/Register'
 import AddEvent from './pages/AddEvent/AddEvent'
 import Event from './pages/Event/scenes/Event'
 import FormikLoginForm from './pages/Login/LoginForm';
+import styled from 'styled-components';
 
 function App () {
   return (
-    <div className='App'>
+    <div>
+       <AppWrapper>
       <h1>Corporate Event Planner</h1>
       <Route exact path="/" component={EventsList}/>
       <Route path="/login" component={FormikLoginForm}/>
       <Route path="/register" component={Register}/>
       <Route path="/event/:id" component={Event}/>
       <Route path="/addevent" component={AddEvent}/>
+      </AppWrapper>
     </div>
   )
 }
 
 export default App
+
+const AppWrapper = styled.div`
+  text-align: center;
+  background: #F9F9F9;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
