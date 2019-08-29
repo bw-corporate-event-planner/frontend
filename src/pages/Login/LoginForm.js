44231867f3edf81d643aa4d1c63e38bbf55f3d20
Annique Nsabimana <anniquensabimana@gmail.com>
2019-08-29 17:46:20 -0500

    fixing forms

--- src/pages/Login/LoginForm.js
+++ src/pages/Login/LoginForm.js
@@ -6,53 +6,50 @@ import axios from 'axios';
 import userContext from '../../contexts/UserContext.js';
 // axios post action 
 
-const LoginForm = ({ errors, touched, values, user, handleSubmit, status, history, ...props }) => {
+const LoginForm = ({ errors, touched, values, handleSubmit, status, ...props}) => {
 
   // hook keeps track of login information 
-  const [login, setLogin] = useState({});
-  const { setUser } = userContext(user);
+  const [login, user, setLogin] = useState({});
+  const {setUser} = userContext(user);
   // update login if change has occured 
   useEffect(() => {
-    if (status) {
-      setUser(status);
-      history.push('/');
-    }
-  }, [status]);
+      if (status) {
+          setUser(status);
+          props.history.push('/');
+      }
+  }, [status]); 
 
-  return (
-    <div className="master-container">
-      <Paper >
-        <h1>Sign In</h1>
-        <Form >
+  return(
+      <div className="master-container">
+          <Paper >
+              <h1>Sign In</h1>
+              <Form >
+                  
+                  {/* name */}
+                  <Field 
+                      type="text" 
+                      name="username" 
+                      placeholder="Userame"  
+                  />
+                  {touched.name && errors.name && ( <p className="error">{errors.name}</p> )}
 
-          {/* name */}
-          <Field
-            type="text"
-            name="username"
-            placeholder="Userame"
-          />
-          {touched.name && errors.name && (<p className="error">{errors.name}</p>)}
-
-				  <div className="field">
                   {/* password */}
                   <Field 
-                      type="password" 
+                      type="text" 
                       name="password" 
                       placeholder="Password" 
                      
                   />
-				  {touched.password && errors.password && <p className="error">{errors.password}</p>}
-				  </div>
-				  
+                  {touched.password && errors.password && <p className="error">{errors.password}</p>}
                   <button type="submit">Submit</button>
               </Form>
-          {/* </Paper> */}
+          </Paper>
       </div>
   );
 };
 // using formik 
 const FormikLoginForm = withFormik({
-
+  
   // making sure each prop has a default value if given value is undefined 
   mapPropsToValues({ username, password, email }) {
     return {
@@ -60,32 +57,32 @@ const FormikLoginForm = withFormik({
       password: password || ""
     };
   },
-
+  
   // use yup to enforce input requirements 
   validationSchema: Yup.object().shape({
-    username: Yup
+      username: Yup
       .string()
       .required("Please Enter Your Name"),
-    password: Yup
+      password: Yup
       .string()
       .required("Please Enter Your Password"),
   }),
-
+  
   // update values and set status 
   handleSubmit(values, { resetForm, props, setStatus }) {
-    console.log("values, props", values, props)
+      console.log("values, props", values, props)
 
-    axios
-      .post("https://egge-corporate-ep.herokuapp.com/api/login", values)
-      .then(response => {
-        console.log(response)
-        console.log('we in there')
-        setStatus(response.data)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-    resetForm();
+      axios
+        .post("https://egge-corporate-ep.herokuapp.com/api/login", values)
+        .then(response => {
+          console.log(response)
+          console.log('we in there')
+          setStatus(response.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      resetForm(); 
   }
 })(LoginForm); // currying functions
 
